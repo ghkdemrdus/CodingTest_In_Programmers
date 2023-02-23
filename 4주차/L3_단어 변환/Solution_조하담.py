bea3fe3a32c7447a77b154e93b6cbcecd4a9143b
조하담 <hadamcho97@gmail.com>
2023-02-23 14:49:07 +0900

    [Refact] L3_단어 변환 - Fix code not to use 'visited' variable - Remove function definition called only once

--- 4주차/L3_단어 변환/Solution_조하담.py
+++ 4주차/L3_단어 변환/Solution_조하담.py
@@ -1,15 +1,7 @@
 from collections import deque
-def solution(begin, target, words):
-    words_set = set(words)
-    answer = 0
-    visited = dict()
-    
-    for w in words:
-        visited[w] = False
-        
-    def find_next_words(word):
+
+def find_next_words(word, words):
         result = []
-        
         for next_word in words:
             difference = 0
             for i in range(len(next_word)):
@@ -17,26 +9,21 @@ def solution(begin, target, words):
                     difference += 1
             if difference == 1:
                 result.append(next_word)
-        
         return result
+    
+def solution(begin, target, words):
+    if target not in words:
+        return 0
 
-    def bfs(start):
-        count = 0
-        q = deque([(start, visited.copy(), count)])
-        visited[start] = True
-        
-        while q:
-            curr_word, curr_visited, count = q.popleft()
-            if curr_word == target:
-                return count
-            next_words = find_next_words(curr_word)
+    count = 0
+    q = deque([(begin, count)])
 
-            for nw in next_words:
-                next_visited = curr_visited.copy()
-                if not curr_visited[nw]:
-                    next_visited[nw] = True
-                    q.append((nw, next_visited, count+1))
-
-    answer = bfs(begin)
+    while q:
+        curr_word, count = q.popleft()
+        if curr_word == target:
+            return count
+        next_words = find_next_words(curr_word, words)
+        for nw in next_words:
+            q.append((nw, count+1))
     
     return answer if answer else 0
